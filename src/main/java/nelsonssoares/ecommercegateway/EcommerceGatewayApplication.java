f0166a301fad32018477d76c4a7d48770edb3884
Nelson <punkpunkada@gmail.com>
2024-02-24 18:14:06 -0300

    strating project

---
+++ src/main/java/nelsonssoares/ecommercegateway/EcommerceGatewayApplication.java
@@ -0,0 +1,15 @@
+package nelsonssoares.ecommercegateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class EcommerceGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceGatewayApplication.class, args);
+	}
+
+}
